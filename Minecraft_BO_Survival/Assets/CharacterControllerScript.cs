9f303a1657c61ba02ea2b0b2eea7b142b54b279c
CalvinWong2003 <cwong.kerry@gmail.com>
2025-01-22 19:19:29 +0000

    Player movement

--- Minecraft_BO_Survival/Assets/CharacterControllerScript.cs
+++ Minecraft_BO_Survival/Assets/CharacterControllerScript.cs
@@ -5,18 +5,86 @@ using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
 {
-    private float speed = 3;
-    float sensitivity = 0.1f;
-    float sensitivity1 = 0.1f;
+    [Header("Movement")]
+    public float speed = 3;
+    public float jumpSpeed = 10f;
+    public float gravity = -10f;
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+
+    public float sensX;
+    public float sensY;
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+
+    public float groundDrag;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        MyInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MyInput()
+    {
+        //mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate camera and orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+        //ground check
+        //grounded = Physics.RayCast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        //handle drag
+        if(grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    private void MovePlayer()
     {
         if (Input.GetKey(KeyCode.W))
         {
@@ -34,10 +102,18 @@ public class CharacterControllerScript : MonoBehaviour
         {
             transform.position -= transform.right * speed * Time.deltaTime;
         }
+    }
 
-        transform.Rotate(Vector3.up,sensitivity* Input.GetAxis("Horizontal"));
-        //transform.Rotate(Vector3.down, sensitivity1 * Input.GetAxis("Vertical"));
-        
+    private void Jump()
+    {
+        //reset Y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
 
